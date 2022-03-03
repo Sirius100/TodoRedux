1d33046550637a6db9793943b01a2d31b51f695a
Sirius100 <esaul-00@mail.ru>
2022-03-03 21:32:09 +0300

    navbar готов

--- app/src/theme/theme.jsx
+++ app/src/theme/theme.jsx
@@ -8,10 +8,11 @@ export const baseTheme = {
     brdDark: 'hsla(0, 100%, 99%, 1)',
   },
   colorFont:{
-    thmLight: 'hsla(190, 88%, 57%, 1);',
-    thmDark:  'hsla(190, 88%, 57%, 1);'
+    thmLight: 'hsla(249, 9%, 29%, 1)',
+    thmDark:  'hsla(190deg 38% 13%)',
   },
   bgBtn: {
-    color: "red",
+    bgLight: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(221,235,232,1) 1%, rgba(188,205,208,1) 100%)',
+    bgDark: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(112,112,173,1) 35%, rgba(0,212,255,1) 100%);'
   }
 }
